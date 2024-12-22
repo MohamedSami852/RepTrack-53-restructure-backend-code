8dd9b9a159e767688db71182ec9814646053469f
Mohamed Sami <119891184+MohamedSami852@users.noreply.github.com>
2024-12-22 07:08:11 +0200

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-fh9-$@*4t7c$5*x9on1-%z!#svehd&m_-egtpkby+1e1!1eul#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['repwebapp-h0hmebebdha5gxdc.canadacentral-01.azurewebsites.net'] 
+ALLOWED_HOSTS = ['repwebapp-h0hmebebdha5gxdc.canadacentral-01.azurewebsites.net','repwebapp-h0hmebebdha5gxdc.canadacentral-01.azurewebsites.net','169.254.129.2','127.0.0.1'] 
 # settings.py
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
